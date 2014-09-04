ec92d57ace5f760a5dcd5fc7c4b199160762a201
Daniel Pink <dr.pink@gmail.com>
2014-09-04 19:04:58 +1000

    Initial Commit

---
+++ SwiftFloatingPointExperiments/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftFloatingPointExperiments
+//
+//  Created by Daniel Pink on 4/09/2014.
+//  Copyright (c) 2014 Daniel Pink. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftFloatingPointExperiments/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftFloatingPointExperiments
+//
+//  Created by Daniel Pink on 4/09/2014.
+//  Copyright (c) 2014 Daniel Pink. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
